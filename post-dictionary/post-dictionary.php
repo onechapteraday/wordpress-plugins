6533637c0ed76c20dc583ac3037d104259c83cc6
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2017-03-26 14:28:02 +0200

    Add new table for post dictionary data

--- post-dictionary/post-dictionary.php
+++ post-dictionary/post-dictionary.php
@@ -13,9 +13,39 @@
 
 
 /**
+ * Create new table for dictionary data
+ *
+ */
+
+function post_dictionary_install_table() {
+    global $wpdb;
+
+    $charset_collate = $wpdb->get_charset_collate();
+    $table_name = $wpdb->prefix . 'postdictionary_data';
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+             id int(11) NOT NULL AUTO_INCREMENT,
+             post_id int(11) NOT NULL,
+             entry text NOT NULL,
+             information text,
+             definition text,
+             PRIMARY KEY  (id)
+           ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    $result = dbDelta( $sql );
+}
+
+# Call the function when the plugin is activated
+
+register_activation_hook( __FILE__, 'post_dictionary_install_table' );
+
+
+/**
  * Add dictionary content at the end of posts
  *
  */
+
 function post_dictionary_add_content($content) {
     if (is_single()) {
         $content .= '<p>The new dictionary will be here!</p>';
