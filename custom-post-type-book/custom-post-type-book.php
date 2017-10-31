1ae9c23da6bc558d433bfd76846ad262232a3007
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2017-11-01 00:48:41 +0100

    Change genre into collection for custom post type book

--- custom-post-type-book/custom-post-type-book.php
+++ custom-post-type-book/custom-post-type-book.php
@@ -94,27 +94,27 @@ add_action( 'pre_get_posts', 'include_book_filter' );
  *
  */
 
-function add_genre_taxonomy() {
+function add_collection_taxonomy() {
   global $BOOK_TEXTDOMAIN;
 
   $labels = array (
-    'name'                       => _x( 'Genres', 'taxonomy general name', $BOOK_TEXTDOMAIN ),
-    'singular_name'              => _x( 'Genre', 'taxonomy singular name', $BOOK_TEXTDOMAIN ),
-    'search_items'               => __( 'Search Genres', $BOOK_TEXTDOMAIN ),
-    'popular_items'              => __( 'Popular Genres', $BOOK_TEXTDOMAIN ),
-    'all_items'                  => __( 'All Genres', $BOOK_TEXTDOMAIN ),
+    'name'                       => _x( 'Collections', 'taxonomy general name', $BOOK_TEXTDOMAIN ),
+    'singular_name'              => _x( 'Collection', 'taxonomy singular name', $BOOK_TEXTDOMAIN ),
+    'search_items'               => __( 'Search Collections', $BOOK_TEXTDOMAIN ),
+    'popular_items'              => __( 'Popular Collections', $BOOK_TEXTDOMAIN ),
+    'all_items'                  => __( 'All Collections', $BOOK_TEXTDOMAIN ),
     'parent_item'                => null,
     'parent_item_colon'          => null,
-    'view_item'                  => __( 'See Genre', $BOOK_TEXTDOMAIN ),
-    'edit_item'                  => __( 'Edit Genre', $BOOK_TEXTDOMAIN ),
-    'update_item'                => __( 'Update Genre', $BOOK_TEXTDOMAIN ),
-    'add_new_item'               => __( 'Add New Genre', $BOOK_TEXTDOMAIN ),
-    'new_item_name'              => __( 'New Genre Name', $BOOK_TEXTDOMAIN ),
-    'separate_items_with_commas' => __( 'Separate genres with commas', $BOOK_TEXTDOMAIN ),
-    'add_or_remove_items'        => __( 'Add or remove genres', $BOOK_TEXTDOMAIN ),
-    'choose_from_most_used'      => __( 'Choose from the most used genres', $BOOK_TEXTDOMAIN ),
-    'not_found'                  => __( 'No genres found.', $BOOK_TEXTDOMAIN ),
-    'menu_name'                  => __( 'Genres', $BOOK_TEXTDOMAIN ),
+    'view_item'                  => __( 'See Collection', $BOOK_TEXTDOMAIN ),
+    'edit_item'                  => __( 'Edit Collection', $BOOK_TEXTDOMAIN ),
+    'update_item'                => __( 'Update Collection', $BOOK_TEXTDOMAIN ),
+    'add_new_item'               => __( 'Add New Collection', $BOOK_TEXTDOMAIN ),
+    'new_item_name'              => __( 'New Collection Name', $BOOK_TEXTDOMAIN ),
+    'separate_items_with_commas' => __( 'Separate collections with commas', $BOOK_TEXTDOMAIN ),
+    'add_or_remove_items'        => __( 'Add or remove collections', $BOOK_TEXTDOMAIN ),
+    'choose_from_most_used'      => __( 'Choose from the most used collections', $BOOK_TEXTDOMAIN ),
+    'not_found'                  => __( 'No collections found.', $BOOK_TEXTDOMAIN ),
+    'menu_name'                  => __( 'Collections', $BOOK_TEXTDOMAIN ),
   );
 
   $args = array (
@@ -124,13 +124,13 @@ function add_genre_taxonomy() {
     'show_admin_column'     => true,
     'update_count_callback' => '_update_post_term_count',
     'query_var'             => true,
-    'rewrite'               => array( 'slug' => 'book/genre', 'with_front' => 'true' ),
+    'rewrite'               => array( 'slug' => 'book/collection', 'with_front' => 'true' ),
   );
 
-  register_taxonomy ('genre', 'book', $args);
+  register_taxonomy ('collection', 'book', $args);
 }
 
-add_action ('init', 'add_genre_taxonomy', 1);
+add_action ('init', 'add_collection_taxonomy', 1);
 
 
 function add_publisher_taxonomy() {
