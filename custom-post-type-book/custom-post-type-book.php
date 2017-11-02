df3763e8da75292c1e117ae4c2eb1a94666c4317
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2017-11-02 08:44:32 +0100

    Add metadata function for custom post type book dates

--- custom-post-type-book/custom-post-type-book.php
+++ custom-post-type-book/custom-post-type-book.php
@@ -224,6 +224,18 @@ function get_book_price ( $post_id ) {
   return get_post_meta($post_id, 'price', true);
 }
 
+function get_book_date_read ( $post_id ) {
+  return get_post_meta($post_id, 'date_release', true);
+}
+
+function get_book_date_release ( $post_id ) {
+  return get_post_meta($post_id, 'date_release', true);
+}
+
+function get_book_date_first_publication ( $post_id ) {
+  return get_post_meta($post_id, 'date_first_publication', true);
+}
+
 function get_book_amazon ( $post_id ) {
   $arr = array(
     'link' => get_post_meta( $post_id, 'amazon', true ),
