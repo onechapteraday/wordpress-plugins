fb205ee6130d398d9b8c018460f90cea7ecf3973
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2017-11-11 22:44:56 +0100

    Add plugin to remove feed links

---
+++ remove-feed-links/remove-feed-links.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * Plugin Name: Remove Feed links
+ * Plugin URI: http://onechapteraday.fr
+ * Description: This plugin removes feed links
+ * Version: 0.1
+ * Author: Christelle Hilaricus
+ * Author URI: http://onechapteraday.fr
+ * License GPL2
+ *
+ */
+
+
+/**
+ * Remove feed links
+ *
+ */
+
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+
+?>
