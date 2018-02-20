c85ad817fcb7659aa4ebb05ed73cb40ef3f97db0
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2018-02-20 11:57:31 +0100

    Sort popular locations widget by considering the accents

--- custom-taxonomy-location/custom-taxonomy-location.php
+++ custom-taxonomy-location/custom-taxonomy-location.php
@@ -600,9 +600,13 @@ class popular_locations_in_category_widget extends WP_Widget {
 
         $locations_array = get_terms ( 'location', $tag_args );
 
-        if ( sizeof ( $locations_array ) ) {
-            function sortByTranslation($a, $b) {
-                return $a->translation > $b->translation;
+        if( sizeof( $locations_array ) ){
+            function sortByTranslation( $a, $b ){
+                $translit = array('Á'=>'A','À'=>'A','Â'=>'A','Ä'=>'A','Ã'=>'A','Å'=>'A','Ç'=>'C','É'=>'E','È'=>'E','Ê'=>'E','Ë'=>'E','Í'=>'I','Ï'=>'I','Î'=>'I','Ì'=>'I','Ñ'=>'N','Ó'=>'O','Ò'=>'O','Ô'=>'O','Ö'=>'O','Õ'=>'O','Ú'=>'U','Ù'=>'U','Û'=>'U','Ü'=>'U','Ý'=>'Y','á'=>'a','à'=>'a','â'=>'a','ä'=>'a','ã'=>'a','å'=>'a','ç'=>'c','é'=>'e','è'=>'e','ê'=>'e','ë'=>'e','í'=>'i','ì'=>'i','î'=>'i','ï'=>'i','ñ'=>'n','ó'=>'o','ò'=>'o','ô'=>'o','ö'=>'o','õ'=>'o','ú'=>'u','ù'=>'u','û'=>'u','ü'=>'u','ý'=>'y','ÿ'=>'y');
+                $at = strtr( $a->translation, $translit );
+                $bt = strtr( $b->translation, $translit );
+
+                return strcoll( $at, $bt );
             }
 
             foreach( $locations_array as $mylocation ) {
