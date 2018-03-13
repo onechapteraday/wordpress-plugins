8b592cea13bc8e47d55dcfa754b5399fd65e3537
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2018-03-13 17:15:55 +0100

    Update the way breadcrumbs are displayed

--- add-breadcrumb-list/add-breadcrumb-list.php
+++ add-breadcrumb-list/add-breadcrumb-list.php
@@ -24,18 +24,16 @@ function create_breadcrumb_single( $atts, $content=null ) {
         <ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
           <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
             <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo get_bloginfo( 'url' ); ?>">
-                <span itemprop="name"><?php echo get_bloginfo( 'name' ); ?></span>
+                <span itemprop="name">Accueil</span>
             </a>
             <meta itemprop="position" content="1" />
           </li>
-          ›
           <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
             <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo get_category_link( $cat_id ); ?>">
               <span itemprop="name"><?php echo $cat_id->cat_name; ?></span>
             </a>
             <meta itemprop="position" content="2" />
           </li>
-          ›
           <li>
               <span itemprop="name"><?php the_title(); ?></span>
           </li>
@@ -59,11 +57,10 @@ function create_breadcrumb_publisher( $atts, $content=null ) {
     <ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
       <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
         <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo get_bloginfo( 'url' ); ?>">
-            <span itemprop="name"><?php echo get_bloginfo( 'name' ); ?></span>
+            <span itemprop="name">Accueil</span>
         </a>
         <meta itemprop="position" content="1" />
       </li>
-      ›
       <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
         <span itemscope itemtype="http://schema.org/Thing" itemprop="item">
           <span itemprop="name">Maisons d'édition</span>
@@ -77,7 +74,6 @@ function create_breadcrumb_publisher( $atts, $content=null ) {
           $parent      = get_term( $parent_id, 'publisher' );
           $parent_link = get_term_link( $parent, 'publisher' );
           ?>
-          ›
           <li>
             <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo $parent_link; ?>">
               <span itemprop="name"><?php echo $parent->name; ?></span>
@@ -87,7 +83,6 @@ function create_breadcrumb_publisher( $atts, $content=null ) {
           <?php
 	}
       ?>
-      ›
       <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
         <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo $publisher_link; ?>">
           <span itemprop="name"><?php echo $publisher->name; ?></span>
