1ebc4fd55329dd5cd13b051f9609c05549869745
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2018-03-13 16:20:03 +0100

    Add breadcrumb list for publishers

--- add-breadcrumb-list/add-breadcrumb-list.php
+++ add-breadcrumb-list/add-breadcrumb-list.php
@@ -3,13 +3,16 @@
  *
  * Plugin Name: Add Breadcrumb List
  * Plugin URI: http://onechapteraday.fr
- * Description: This plugin creates a breadcrumb list to use in single posts
+ * Description: This plugin creates a breadcrumb list for pages
  * Version: 0.1
  * Author: Christelle Hilaricus
  * Author URI: http://onechapteraday.fr
  * License GPL2
  *
  */
+
+
+# Create breadcrumb for single posts
 
 function create_breadcrumb_single( $atts, $content=null ) {
     $post_id = $atts['id'];
@@ -42,5 +45,59 @@ function create_breadcrumb_single( $atts, $content=null ) {
 }
 
 add_shortcode( 'wp_breadcrumb_single', 'create_breadcrumb_single' );
+
+
+# Create breadcrumb for publishers
+
+function create_breadcrumb_publisher( $atts, $content=null ) {
+    $publisher_id = $atts['id'];
+
+    $publisher      = get_term( $publisher_id, 'publisher' );
+    $publisher_link = get_term_link( $publisher, 'publisher' );
+
+    ?>
+    <ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
+      <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+        <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo get_bloginfo( 'url' ); ?>">
+            <span itemprop="name"><?php echo get_bloginfo( 'name' ); ?></span>
+        </a>
+        <meta itemprop="position" content="1" />
+      </li>
+      ›
+      <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+        <span itemscope itemtype="http://schema.org/Thing" itemprop="item">
+          <span itemprop="name">Maisons d'édition</span>
+        </span>
+        <meta itemprop="position" content="2" />
+      </li>
+      <?php
+	if( $publisher->parent > 0 ){
+	  $parent_id = $publisher->parent;
+
+          $parent      = get_term( $parent_id, 'publisher' );
+          $parent_link = get_term_link( $parent, 'publisher' );
+          ?>
+          ›
+          <li>
+            <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo $parent_link; ?>">
+              <span itemprop="name"><?php echo $parent->name; ?></span>
+            </a>
+            <meta itemprop="position" content="3" />
+          </li>
+          <?php
+	}
+      ?>
+      ›
+      <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+        <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo $publisher_link; ?>">
+          <span itemprop="name"><?php echo $publisher->name; ?></span>
+        </a>
+        <meta itemprop="position" content="<?php if( $publisher->parent > 0 ){ echo '4'; }else{ echo '3'; } ?>" />
+      </li>
+    </ol>
+    <?php
+}
+
+add_shortcode( 'wp_breadcrumb_publisher', 'create_breadcrumb_publisher' );
 
 ?>
