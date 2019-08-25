ec82e31a745722cab22075718d87bb165aa5f484
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2019-08-25 15:02:09 -0400

    Translate dashboard widgets of custom taxonomy person

--- custom-taxonomy-person/admin/dashboard_widgets.php
+++ custom-taxonomy-person/admin/dashboard_widgets.php
@@ -24,7 +24,7 @@ function birthdays_dashboard_widget(){
     $id = $person->term_id;
     $term_meta = get_option( 'taxonomy_' . $id );
 
-    if( $term_meta['birthdate'] != '' ){
+    if( $term_meta[ 'birthdate' ] != '' ){
       $birthdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'birthdate' ] );
 
       if( $month == $birthdate->format( 'm' ) ){
@@ -64,8 +64,10 @@ function birthdays_dashboard_widget(){
 
         # Retrieve all custom data
         $term_meta = get_option( 'taxonomy_' . $id );
+        $gender    = $term_meta[ 'gender' ];
         $birthdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'birthdate' ] );
         $deathdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'deathdate' ] );
+
         if( $deathdate == '' ) $age = $now->diff( $birthdate )->y;
 	else $age = $deathdate->diff( $birthdate )->y;
 
@@ -78,9 +80,26 @@ function birthdays_dashboard_widget(){
 	      <?php echo $name; ?>
             </a>
           </td>
-          <td><small><?php echo $age . ' years old'; ?></small></td>
+          <td><small><?php echo $age . __( ' years old', 'person-taxonomy' ); ?></small></td>
           <td>
-	      <small><em><?php echo 'born on ' . $birthdate->format( 'Y' ); if( $deathdate != '' ){ echo ', deceased on ' . $deathdate->format( 'Y' ); } ?></em></small>
+	      <small><em><?php
+                  if( $gender == 1){
+                      echo _x( 'born on ', 'year of birth date for female', 'person-taxonomy' );
+                  } else {
+                      echo _x( 'born on ', 'year of birth date for male', 'person-taxonomy' );
+                  }
+
+                  echo $birthdate->format( 'Y' );
+
+                  if( $deathdate != '' ){
+                      if( $gender == 1){
+                          echo _x( ', deceased on ', 'year of death date for female', 'person-taxonomy' );
+                      } else {
+                          echo _x( ', deceased on ', 'year of death date for male', 'person-taxonomy' );
+                      }
+
+                      echo $deathdate->format( 'Y' );
+                  } ?></em></small>
 	  </td>
         </tr>
         <?php
@@ -89,7 +108,7 @@ function birthdays_dashboard_widget(){
     </table>
     <?php
   } else {
-    echo 'There is nobody born this month among your persons.';
+    echo __( 'There is nobody born this month among your persons.', 'person-taxonomy' );
   }
 }
 
@@ -159,8 +178,10 @@ function deathdays_dashboard_widget(){
 
         # Retrieve all custom data
         $term_meta = get_option( 'taxonomy_' . $id );
+        $gender    = $term_meta[ 'gender' ];
         $birthdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'birthdate' ] );
         $deathdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'deathdate' ] );
+
         $now = new DateTime();
 	$age = $deathdate->diff( $birthdate )->y;
 
@@ -173,19 +194,30 @@ function deathdays_dashboard_widget(){
 	      <?php echo $name; ?>
             </a>
           </td>
-          <td><small><?php echo $age . ' years old'; ?></small></td>
+          <td><small><?php echo $age . __( ' years old', 'person-taxonomy' ); ?></small></td>
           <td>
 	      <small><em><?php
-                  echo 'died on ' . $deathdate->format( 'Y' );
-
                   if( $deathdate != '' ){
                       $since_date = $now->diff( $deathdate )->y;
-                      echo ', deceased ' . $since_date;
 
-                      if( $since_date > 1 ){
-                          echo ' years ago';
+                      if( $gender == 1){
+                          echo _x( 'deceased on ', 'year of date for female', 'person-taxonomy' );
+                          echo $deathdate->format( 'Y' );
+
+                          if( $since_date > 1 ){
+                              printf( __( ', %s years ago', 'person-taxonomy' ), $since_date );
+                          } else {
+                              printf( __( ', %s year ago', 'person-taxonomy' ), $since_date );
+                          }
                       } else {
-                          echo ' year ago';
+                          echo _x( 'deceased on ', 'year of date for male', 'person-taxonomy' );
+                          echo $deathdate->format( 'Y' );
+
+                          if( $since_date > 1 ){
+                              printf( __( ', %s years ago', 'person-taxonomy' ), $since_date );
+                          } else {
+                              printf( __( ', %s year ago', 'person-taxonomy' ), $since_date );
+                          }
                       }
                   }
               ?></em></small>
@@ -197,7 +229,7 @@ function deathdays_dashboard_widget(){
     </table>
     <?php
   } else {
-    echo 'There is nobody who died this month among your persons.';
+    echo __( 'There is nobody who died this month among your persons.', 'person-taxonomy' );
   }
 }
 
@@ -205,11 +237,11 @@ function deathdays_dashboard_widget(){
 function add_dashboard_widgets(){
   $now = new DateTime();
 
-  $birthdays_title       = $now->format( 'F' ) . ' birthdays';
+  $birthdays_title       = __( 'Birthdays of the month', 'person-taxonomy' );
   $birthdays_function    = 'birthdays_dashboard_widget';
   $birthdays_widget_slug = 'birthdays-dashboard-widget';
 
-  $deathdays_title       = 'Death anniversaries of the month';
+  $deathdays_title       = __( 'Death anniversaries of the month', 'person-taxonomy' );
   $deathdays_function    = 'deathdays_dashboard_widget';
   $deathdays_widget_slug = 'deathdays-dashboard-widget';
 
