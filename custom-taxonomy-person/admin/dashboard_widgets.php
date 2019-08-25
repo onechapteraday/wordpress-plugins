333408bb034fc0ce27c9661537e7e72a3b6d55f9
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2019-08-25 14:21:01 -0400

    Update coding styles

--- custom-taxonomy-person/admin/dashboard_widgets.php
+++ custom-taxonomy-person/admin/dashboard_widgets.php
@@ -4,73 +4,75 @@
  *
  */
 
-function birthdays_dashboard_widget () {
+function birthdays_dashboard_widget(){
   # Get all persons created
-  $terms = get_terms(array(
+  $terms = get_terms( array(
     'taxonomy' => 'person',
     'hide_empty' => false,
     'fields' => 'all',
     'limit' => -1,
-  ));
+  ) );
 
   # Get today's month and day
   $now = new DateTime();
-  $month = date('m');
-  $day = date('d');
+  $month = date( 'm' );
+  $day = date( 'd' );
   $results = array();
 
   # Retrieve all persons born this month
-  foreach ($terms as $person) {
+  foreach( $terms as $person ){
     $id = $person->term_id;
     $term_meta = get_option( 'taxonomy_' . $id );
-    if ($term_meta['birthdate'] != '') {
-      $birthdate = DateTime::createFromFormat('Y-m-d', $term_meta['birthdate']);
-      if ($month == $birthdate->format('m')) {
-        array_push($results, $person);
+
+    if( $term_meta['birthdate'] != '' ){
+      $birthdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'birthdate' ] );
+
+      if( $month == $birthdate->format( 'm' ) ){
+        array_push( $results, $person );
       }
     }
   }
 
   # If array has results
-  $count = sizeof($results);
+  $count = sizeof( $results );
 
-  if($count > 0) {
+  if( $count > 0 ){
     # Sort array by day number
-    uasort($results, function($a, $b) {
+    uasort( $results, function( $a, $b ){
       $a_id = $a->term_id;
       $b_id = $b->term_id;
       $a_meta = get_option( 'taxonomy_' . $a_id );
       $b_meta = get_option( 'taxonomy_' . $b_id );
-      $a_date = DateTime::createFromFormat('Y-m-d', $a_meta['birthdate']);
-      $b_date = DateTime::createFromFormat('Y-m-d', $b_meta['birthdate']);
-      $a_str = $a_date->format('j');
-      $b_str = $b_date->format('j');
+      $a_date = DateTime::createFromFormat( 'Y-m-d', $a_meta[ 'birthdate' ] );
+      $b_date = DateTime::createFromFormat( 'Y-m-d', $b_meta[ 'birthdate' ] );
+      $a_str = $a_date->format( 'j' );
+      $b_str = $b_date->format( 'j' );
 
-      return ($a_str < $b_str) ? -1 : 1;
+      return ( $a_str < $b_str ) ? -1 : 1;
     });
 
     # Display all persons with their birthday and age
     ?>
     <table cellspacing="0" cellpadding="3" width="100%">
       <?php
-      foreach ($results as $person) {
+      foreach( $results as $person ){
         $id = $person->term_id;
         $name = $person->name;
         $slug = $person->slug;
         $description = $person->description;
-        $link = get_term_link($person);
+        $link = get_term_link( $person );
 
         # Retrieve all custom data
         $term_meta = get_option( 'taxonomy_' . $id );
-        $birthdate = DateTime::createFromFormat('Y-m-d', $term_meta['birthdate']);
-        $deathdate = DateTime::createFromFormat('Y-m-d', $term_meta['deathdate']);
-        if ($deathdate == '') $age = $now->diff($birthdate)->y;
-	else $age = $deathdate->diff($birthdate)->y;
+        $birthdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'birthdate' ] );
+        $deathdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'deathdate' ] );
+        if( $deathdate == '' ) $age = $now->diff( $birthdate )->y;
+	else $age = $deathdate->diff( $birthdate )->y;
 
 	# Display data
         ?>
-        <tr<?php if ($day == $birthdate->format('d')) { ?> class="highlight"<?php } ?>>
-	  <td><?php echo $birthdate->format('M. j'); ?></td>
+        <tr<?php if( $day == $birthdate->format( 'd' ) ) { ?> class="highlight"<?php } ?>>
+	  <td><?php echo $birthdate->format( 'M. j' ); ?></td>
           <td>
             <a href="<?php echo $link; ?>" target="blank">
 	      <?php echo $name; ?>
@@ -78,7 +80,7 @@ function birthdays_dashboard_widget () {
           </td>
           <td><small><?php echo $age . ' years old'; ?></small></td>
           <td>
-	      <small><em><?php echo 'born on ' . $birthdate->format('Y'); if ($deathdate != '') { echo ', deceased on ' . $deathdate->format('Y'); } ?></em></small>
+	      <small><em><?php echo 'born on ' . $birthdate->format( 'Y' ); if( $deathdate != '' ){ echo ', deceased on ' . $deathdate->format( 'Y' ); } ?></em></small>
 	  </td>
         </tr>
         <?php
@@ -97,73 +99,75 @@ function birthdays_dashboard_widget () {
  *
  */
 
-function deathdays_dashboard_widget () {
+function deathdays_dashboard_widget(){
   # Get all persons created
-  $terms = get_terms(array(
+  $terms = get_terms( array(
     'taxonomy' => 'person',
     'hide_empty' => false,
     'fields' => 'all',
     'limit' => -1,
-  ));
+  ) );
 
   # Get today's month and day
   $now = new DateTime();
-  $month = date('m');
-  $day = date('d');
+  $month = date( 'm' );
+  $day = date( 'd' );
   $results = array();
 
   # Retrieve all persons who died this month
-  foreach ($terms as $person) {
+  foreach( $terms as $person ){
     $id = $person->term_id;
     $term_meta = get_option( 'taxonomy_' . $id );
-    if ($term_meta['deathdate'] != '') {
-      $deathdate = DateTime::createFromFormat('Y-m-d', $term_meta['deathdate']);
-      if ($month == $deathdate->format('m')) {
-        array_push($results, $person);
+
+    if( $term_meta[ 'deathdate' ] != '' ){
+      $deathdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'deathdate' ] );
+
+      if( $month == $deathdate->format( 'm' ) ){
+        array_push( $results, $person );
       }
     }
   }
 
   # If array has results
-  $count = sizeof($results);
+  $count = sizeof( $results );
 
-  if($count > 0) {
+  if( $count > 0 ){
     # Sort array by day number
-    uasort($results, function($a, $b) {
+    uasort( $results, function( $a, $b ){
       $a_id = $a->term_id;
       $b_id = $b->term_id;
       $a_meta = get_option( 'taxonomy_' . $a_id );
       $b_meta = get_option( 'taxonomy_' . $b_id );
-      $a_date = DateTime::createFromFormat('Y-m-d', $a_meta['deathdate']);
-      $b_date = DateTime::createFromFormat('Y-m-d', $b_meta['deathdate']);
-      $a_str = $a_date->format('j');
-      $b_str = $b_date->format('j');
+      $a_date = DateTime::createFromFormat( 'Y-m-d', $a_meta[ 'deathdate' ] );
+      $b_date = DateTime::createFromFormat( 'Y-m-d', $b_meta[ 'deathdate' ] );
+      $a_str = $a_date->format( 'j ');
+      $b_str = $b_date->format( 'j ');
 
-      return ($a_str < $b_str) ? -1 : 1;
-    });
+      return ( $a_str < $b_str ) ? -1 : 1;
+    } );
 
     # Display all persons with their deathday and age
     ?>
     <table cellspacing="0" cellpadding="3" width="100%">
       <?php
-      foreach ($results as $person) {
+      foreach( $results as $person ){
         $id = $person->term_id;
         $name = $person->name;
         $slug = $person->slug;
         $description = $person->description;
-        $link = get_term_link($person);
+        $link = get_term_link( $person );
 
         # Retrieve all custom data
         $term_meta = get_option( 'taxonomy_' . $id );
-        $birthdate = DateTime::createFromFormat('Y-m-d', $term_meta['birthdate']);
-        $deathdate = DateTime::createFromFormat('Y-m-d', $term_meta['deathdate']);
+        $birthdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'birthdate' ] );
+        $deathdate = DateTime::createFromFormat( 'Y-m-d', $term_meta[ 'deathdate' ] );
         $now = new DateTime();
-	$age = $deathdate->diff($birthdate)->y;
+	$age = $deathdate->diff( $birthdate )->y;
 
 	# Display data
         ?>
-        <tr<?php if ($day == $deathdate->format('d')) { ?> class="highlight"<?php } ?>>
-	  <td><?php echo $deathdate->format('M. j'); ?></td>
+        <tr<?php if( $day == $deathdate->format( 'd' ) ) { ?> class="highlight"<?php } ?>>
+	  <td><?php echo $deathdate->format( 'M. j' ); ?></td>
           <td>
             <a href="<?php echo $link; ?>" target="blank">
 	      <?php echo $name; ?>
@@ -198,7 +202,7 @@ function deathdays_dashboard_widget () {
 }
 
 
-function add_dashboard_widgets() {
+function add_dashboard_widgets(){
   $now = new DateTime();
 
   $birthdays_title       = $now->format( 'F' ) . ' birthdays';
