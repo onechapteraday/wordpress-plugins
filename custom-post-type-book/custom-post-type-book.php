52d712e77c0aea01852d79208ab9c0d4a4f6387d
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2019-08-25 08:52:00 -0400

    Update coding style

--- custom-post-type-book/custom-post-type-book.php
+++ custom-post-type-book/custom-post-type-book.php
@@ -22,7 +22,7 @@ $BOOK_TEXTDOMAIN = 'book-post-type';
  *
  */
 
-function book_taxonomy_load_textdomain() {
+function book_taxonomy_load_textdomain(){
   global $BOOK_TEXTDOMAIN;
   $locale = apply_filters( 'plugin_locale', get_locale(), $BOOK_TEXTDOMAIN );
 
@@ -39,7 +39,7 @@ add_action( 'init', 'book_taxonomy_load_textdomain', 0 );
  *
  */
 
-function create_post_type_book() {
+function create_post_type_book(){
   global $BOOK_TEXTDOMAIN;
 
   register_post_type( 'book',
@@ -99,14 +99,15 @@ add_action( 'init', 'create_post_type_book' );
  *
  */
 
-function add_custom_post_type_book_to_dashboard_activity( $query_args ) {
-	if ( is_array( $query_args[ 'post_type' ] ) ) {
-		//Set yout post type
-		$query_args[ 'post_type' ][] = 'book';
+function add_custom_post_type_book_to_dashboard_activity( $query_args ){
+	if( is_array( $query_args[ 'post_type' ] ) ){
+	    # Set yout post type
+	    $query_args[ 'post_type' ][] = 'book';
 	} else {
-		$temp = array( $query_args[ 'post_type' ], 'book' );
-		$query_args[ 'post_type' ] = $temp;
+	    $temp = array( $query_args[ 'post_type' ], 'book' );
+	    $query_args[ 'post_type' ] = $temp;
 	}
+
 	return $query_args;
 }
 
@@ -118,7 +119,7 @@ add_filter( 'dashboard_recent_posts_query_args', 'add_custom_post_type_book_to_d
  *
  */
 
-function custom_post_type_book_at_a_glance() {
+function custom_post_type_book_at_a_glance(){
     $args = array(
         'name'     => 'book',
         '_builtin' => false,
@@ -126,12 +127,12 @@ function custom_post_type_book_at_a_glance() {
 
     $object = get_post_types( $args, 'objects' );
 
-    foreach ( $object as $post_type ) {
+    foreach( $object as $post_type ){
         $num_posts = wp_count_posts( $post_type->name );
         $num = number_format_i18n( $num_posts->publish );
         $text = _n( strtolower( $post_type->labels->singular_name ), strtolower( $post_type->labels->name ), $num_posts->publish );
 
-        if ( current_user_can( 'edit_posts' ) ) {
+        if( current_user_can( 'edit_posts' ) ){
             $num = '<li class="post-count custom-post-type-book"><a href="edit.php?post_type=' . $post_type->name . '">' . $num . ' ' . $text . '</a></li>';
         }
 
@@ -147,51 +148,51 @@ add_action( 'dashboard_glance_items', 'custom_post_type_book_at_a_glance' );
  *
  */
 
-function get_book_cover ( $post_id ) {
-  return get_post_meta($post_id, 'cover', true);
+function get_book_cover( $post_id ){
+  return get_post_meta( $post_id, 'cover', true );
 }
 
-function get_book_excerpt ( $post_id ) {
-  return get_post_meta($post_id, 'excerpt', true);
+function get_book_excerpt( $post_id ){
+  return get_post_meta( $post_id, 'excerpt', true );
 }
 
-function get_book_title_read ( $post_id ) {
-  return get_post_meta($post_id, 'title_read', true);
+function get_book_title_read( $post_id ){
+  return get_post_meta( $post_id, 'title_read', true );
 }
 
-function get_book_title_original ( $post_id ) {
-  return get_post_meta($post_id, 'title_original', true);
+function get_book_title_original( $post_id ){
+  return get_post_meta( $post_id, 'title_original', true );
 }
 
-function get_book_pages_number ( $post_id ) {
-  return get_post_meta($post_id, 'pages_number', true);
+function get_book_pages_number( $post_id ){
+  return get_post_meta( $post_id, 'pages_number', true );
 }
 
-function get_book_isbn13 ( $post_id ) {
-  return get_post_meta($post_id, 'isbn13', true);
+function get_book_isbn13( $post_id ){
+  return get_post_meta( $post_id, 'isbn13', true );
 }
 
-function get_book_price ( $post_id ) {
-  return get_post_meta($post_id, 'price', true);
+function get_book_price( $post_id ){
+  return get_post_meta( $post_id, 'price', true );
 }
 
-function get_book_date_read ( $post_id ) {
-  return get_post_meta($post_id, 'date_read', true);
+function get_book_date_read( $post_id ){
+  return get_post_meta( $post_id, 'date_read', true );
 }
 
-function get_book_date_release ( $post_id ) {
-  return get_post_meta($post_id, 'date_release', true);
+function get_book_date_release( $post_id ){
+  return get_post_meta( $post_id, 'date_release', true );
 }
 
-function get_book_date_first_publication ( $post_id ) {
-  return get_post_meta($post_id, 'date_first_publication', true);
+function get_book_date_first_publication( $post_id ){
+  return get_post_meta( $post_id, 'date_first_publication', true );
 }
 
-function get_book_rating ( $post_id ) {
-  return get_post_meta($post_id, 'rating', true);
+function get_book_rating( $post_id ){
+  return get_post_meta( $post_id, 'rating', true );
 }
 
-function get_book_amazon ( $post_id ) {
+function get_book_amazon( $post_id ){
   $arr = array(
     'link'    => get_post_meta( $post_id, 'amazon', true ),
     'img'     => plugin_dir_url( __FILE__ ) . 'images/logo_amazon.png',
@@ -201,7 +202,7 @@ function get_book_amazon ( $post_id ) {
   return $arr;
 }
 
-function get_book_fnac ( $post_id ) {
+function get_book_fnac( $post_id ){
   $arr = array(
     'link' => get_post_meta( $post_id, 'fnac', true ),
     'img' => plugin_dir_url( __FILE__ ) . 'images/logo_fnac.png'
@@ -210,7 +211,7 @@ function get_book_fnac ( $post_id ) {
   return $arr;
 }
 
-function get_book_author ( $post_id ) {
+function get_book_author( $post_id ){
   if( taxonomy_exists( 'person' ) ){
     $authors = array();
     $persons = get_post_meta( $post_id, 'author', true );
@@ -227,7 +228,7 @@ function get_book_author ( $post_id ) {
   }
 }
 
-function get_book_author_preface ( $post_id ) {
+function get_book_author_preface( $post_id ){
   if( taxonomy_exists( 'person' ) ){
     $preface_authors = array();
     $persons = get_post_meta( $post_id, 'author_preface', true );
@@ -244,7 +245,7 @@ function get_book_author_preface ( $post_id ) {
   }
 }
 
-function get_book_translator ( $post_id ) {
+function get_book_translator( $post_id ){
   if( taxonomy_exists( 'person' ) ){
     $translators = array();
     $persons = get_post_meta( $post_id, 'translator', true );
@@ -261,7 +262,7 @@ function get_book_translator ( $post_id ) {
   }
 }
 
-function get_book_illustrator ( $post_id ) {
+function get_book_illustrator( $post_id ){
   if( taxonomy_exists( 'person' ) ){
     $illustrators = array();
     $persons = get_post_meta( $post_id, 'illustrator', true );
@@ -278,7 +279,7 @@ function get_book_illustrator ( $post_id ) {
   }
 }
 
-function get_book_colourist ( $post_id ) {
+function get_book_colourist( $post_id ){
   if( taxonomy_exists( 'person' ) ){
     $colourists = array();
     $persons = get_post_meta( $post_id, 'colourist', true );
@@ -295,13 +296,13 @@ function get_book_colourist ( $post_id ) {
   }
 }
 
-function get_book_publisher ( $post_id ) {
+function get_book_publisher( $post_id ){
   if( taxonomy_exists( 'publisher' ) ){
     $publisher = get_the_terms( $post_id, 'publisher' )[0];
 
     # If parent
-    if ( isset ( $publisher->parent ) ) {
-        if ( $publisher->parent ) {
+    if( isset ( $publisher->parent ) ){
+        if( $publisher->parent ){
             return get_term_by( 'id', $publisher->parent, 'publisher' );
         }
     }
@@ -310,13 +311,13 @@ function get_book_publisher ( $post_id ) {
   }
 }
 
-function get_book_collection ( $post_id ) {
+function get_book_collection( $post_id ){
   if( taxonomy_exists( 'publisher' ) ){
     $publisher = get_the_terms( $post_id, 'publisher' )[0];
 
     # If parent
-    if ( isset( $publisher->parent ) ) {
-        if ( $publisher->parent ) {
+    if( isset( $publisher->parent ) ){
+        if( $publisher->parent ){
             return $publisher;
         }
     }
