ef746f6579189f39663bc8d95d50f8baec155817
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2019-08-25 08:54:12 -0400

    Add ISBN10 support

--- custom-post-type-book/custom-post-type-book.php
+++ custom-post-type-book/custom-post-type-book.php
@@ -168,6 +168,10 @@ function get_book_pages_number( $post_id ){
   return get_post_meta( $post_id, 'pages_number', true );
 }
 
+function get_book_isbn10( $post_id ){
+  return get_post_meta( $post_id, 'isbn10', true );
+}
+
 function get_book_isbn13( $post_id ){
   return get_post_meta( $post_id, 'isbn13', true );
 }
